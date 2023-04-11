2edf5067cb36ee857f594d083640a97cb4bfd200
Joe Gastring <Joe.Gastring@DBMGlobal.com>
2023-04-11 09:39:20 -0500

    Initial

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
 namespace SESReportsTest
 {
     public class UnitTest1
@@ -5,7 +8,12 @@ namespace SESReportsTest
         [Fact]
         public void Test1()
         {
+            // Browser Driver
+            IWebDriver webDriver = new EdgeDriver();
 
+            // Navigate To Site
+            webDriver.Navigate().GoToUrl("https://sesqa.schuff.com/");
+            
         }
     }
 }
